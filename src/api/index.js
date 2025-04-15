016c2fbc2c946ebdaedb90b9ecf6d60760ff9cb0
Bottomloader <imsamlolll@gmail.com>
2025-04-15 22:34:41 +0300

    Lowercase test

--- src/api/index.js
+++ src/api/index.js
@@ -2,17 +2,31 @@ export default function handler(req, res) {
   const { functionname } = req.query;
 
   const functions = {
-    applicationCommandCount: () => ({
+    applicationcommandcount: () => ({
       info: {
         name: "$applicationCommandCount",
         description: "Returns the number of registered global slash commands.",
         args: "none"
       }
     }),
+    applicationcommandlist: () => ({
+      info: {
+        name: "$applicationCommandList",
+        description: "Returns a semicolon-separated list of registered global slash command names.",
+        args: "none"
+      }
+    }),
+    applicationownerid: () => ({
+      info: {
+        name: "$applicationOwnerID",
+        description: "Returns the user ID of the application owner.",
+        args: "none"
+      }
+    }),
   };
 
-  if (functions[functionname]) {
-    res.status(200).json(functions[functionname]());
+  if (functions[functionname.toLowerCase()]) {
+    res.status(200).json(functions[functionname.toLowerCase()]());
   } else {
     res.status(200).json({ error: "Function does not exist" });
   }
