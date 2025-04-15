d8ec8bbd0b4e1f60dc07859d6b5d426fb41319df
Bottomloader <imsamlolll@gmail.com>
2025-04-15 22:54:57 +0300

    Too lazy to contiune

--- src/api/index.js
+++ src/api/index.js
@@ -6,21 +6,56 @@ export default function handler(req, res) {
       info: {
         name: "$applicationCommandCount",
         description: "Returns the number of registered global slash commands.",
-        args: "none"
+        args: "none",
+        alias: "none"
       }
     }),
     applicationcommandlist: () => ({
       info: {
         name: "$applicationCommandList",
         description: "Returns a semicolon-separated list of registered global slash command names.",
-        args: "none"
+        args: "none",
+        alias: "none"
       }
     }),
     applicationownerid: () => ({
       info: {
         name: "$applicationOwnerID",
         description: "Returns the user ID of the application owner.",
-        args: "none"
+        args: "none",
+        alias: "none"
+      }
+    }),
+    botavatar: () => ({
+      info: {
+        name: "$botAvatar",
+        description: "Returns the bot's avatar URL.",
+        args: "none",
+        alias: "none"
+      }
+    }),
+    botguildscount: () => ({
+      info: {
+        name: "$botGuildsCount",
+        description: "Returns the number of guilds the bot is in.",
+        args: "none",
+        alias: "none"
+      }
+    }),
+    botid: () => ({
+      info: {
+        name: "$botID",
+        description: "Returns the bot's user ID.",
+        args: "none",
+        alias: "$clientID"
+      }
+    }),
+    clientid: () => ({
+      info: {
+        name: "$clientID",
+        description: "Returns the bot's user ID.",
+        args: "none",
+        alias: "$botID"
       }
     }),
   };
