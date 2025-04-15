660b06fd3f1555c40b3e37c5a1b78981c6e60f42
Bottomloader <imsamlolll@gmail.com>
2025-04-15 23:21:08 +0300

    Got lazy again

--- src/api/index.js
+++ src/api/index.js
@@ -58,11 +58,83 @@ export default function handler(req, res) {
         alias: "$botID"
       }
     }),
+    botinviteurl: () => ({
+      info: {
+        name: "$botInviteURL",
+        description: "Generates the bot's invite URL with specified permissions using either a bitfield number or permission names.",
+        args: "[permissionBitfield or permName1;permName2...?]",
+        alias: "none"
+      }
+    }),
+    botleave: () => ({
+      info: {
+        name: "$botLeave",
+        description: "Makes the bot leave the specified guild.",
+        args: "[guildID]",
+        alias: "none"
+      }
+    }),
+    botname: () => ({
+      info: {
+        name: "$botName",
+        description: "Returns the bot's username.",
+        args: "none",
+        alias: "none"
+      }
+    }),
+    botping: () => ({
+      info: {
+        name: "$botPing",
+        description: "Returns websocket ping in milliseconds.",
+        args: "none",
+        alias: "$ping"
+      }
+    }),
+    ping: () => ({
+      info: {
+        name: "$ping",
+        description: "Returns websocket ping in milliseconds.",
+        args: "none",
+        alias: "$botPing"
+      }
+    }),
+    botpresenceactivityname: () => ({
+      info: {
+        name: "$botPresenceActivityName",
+        description: "Returns the name of the bot's current primary activity.",
+        args: "none",
+        alias: "none"
+      }
+    }),
+    botpresenceactivitytype: () => ({
+      info: {
+        name: "$botPresenceActivityType",
+        description: "Returns the type of the bot's current primary activity (like Playing, Watching, etc.).",
+        args: "none",
+        alias: "none"
+      }
+    }),
+    botpresencestatus: () => ({
+      info: {
+        name: "$botPresenceStatus",
+        description: "Returns the bot's current presence status (like online, idle, etc.).",
+        args: "none",
+        alias: "none"
+      }
+    }),
+    bottags: () => ({
+      info: {
+        name: "$botTags",
+        description: "Returns semicolon-separated application tags.",
+        args: "none",
+        alias: "none"
+      }
+    }),
   };
 
   if (functions[functionname.toLowerCase()]) {
     res.status(200).json(functions[functionname.toLowerCase()]());
   } else {
-    res.status(200).json({ error: "Function does not exist" });
+    res.status(404).json({ error: "Function does not exist" });
   }
 }
